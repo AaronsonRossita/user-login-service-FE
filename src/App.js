e5b04308753e768327e50da2edfa097ae07feee5
benmeirr <benmeir615@gmail.com>
2022-12-03 18:13:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,82 +1,14 @@
-import React, {useState} from "react";
-import './App.css';
-import RegisteredStudent from "./components/studentRegistrations/RegisteredStudent";
-import NewStudent from "./components/newStudent/NewStudent";
-import Registrations from "./components/studentRegistrations/Registrations";
+import React from "react";
+import Login from "./components/registration/Login";
 
-const AVAILABLE_COURSES = {
-    fullstack: {
-        id: '1',
-        name: 'fullstack',
-        displayName: 'Fullstack Course',
-        startDate: new Date(2023, 7, 14)
-    },
-    qa: {
-        id: '2',
-        name: 'qa',
-        displayName: 'QA Course',
-        startDate: new Date(2023, 2, 20)
-    },
-    cyber: {
-        id: '3',
-        name: 'cyber',
-        displayName: 'Cyber Course',
-        startDate: new Date(2023, 4, 5)
-    },
-    product: {
-        id: '4',
-        name: 'product',
-        displayName: 'Product Course',
-        startDate: new Date(2023, 6, 30)
-    },
+
+function App4() {
+
+    return (
+        <main className="App">
+            <Login />
+        </main>
+    );
 }
 
-const DUMMY_REGISTERED_STUDENTS = [
-    {
-        id: '1',
-        studentName: "Yonit Levi",
-        course: "fullstack",
-    },
-    { id: '2',
-        studentName: "Eli Finish",
-        course: "qa",
-    },
-    {
-        id: '3',
-        studentName: "Eyal Golan",
-        course: "cyber",
-    },
-    {
-        id: '4',
-        studentName: "Oren Hazan",
-        course: "fullstack",
-    },
-    {
-        id: '5',
-        studentName: "Rotem Sela",
-        course: "product",
-    },
-];
-
-function App() {
-
-  const [registeredStudents, setRegisteredStudents] = useState(DUMMY_REGISTERED_STUDENTS);
-
-
-  const addStudentHandler = (registeredStudent) => {
-      setRegisteredStudents((prevStudents => {
-          return [registeredStudent, ...prevStudents]
-      }));
-  }
-
-  return(
-    <div>
-      <NewStudent onRegisteredNewStudent={addStudentHandler} courses={AVAILABLE_COURSES}/>
-      <Registrations registeredStudents={registeredStudents} courses={AVAILABLE_COURSES}/>
-    </div>
-  ) 
-}
-
-export default App;
-
-
+export default App4;
---
+++ src/components/registration/Login.js
@@ -0,0 +1,81 @@
+import React, {useRef, useState, useEffect, useContext, Fragment} from 'react';
+import AuthContext from "../../context/AuthProvider";
+import classes from "./Login.module.css";
+
+
+function Login() {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+
+    return(
+        <Fragment>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="#">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? classes.error_mes : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="userName">User Name:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                        />
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                        />
+                        <button type="submit">Sign In</button>
+                    </form>
+                    <p>
+                        Need an Account?<br />
+                        <span className="line">
+                            {/*put router link here*/}
+                            <a href="#">Sign Up</a>
+                                </span>
+                    </p>
+                </section>
+            )
+            }
+        </Fragment>
+    )
+}
+
+export default Login;
